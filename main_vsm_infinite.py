1482e7d0f65ea366ec66da546aa730e741838e8c
QinKevin-001 <zihaoqin1via@gmail.com>
2025-02-24 13:52:06 -0500

    tested

--- main_vsm_infinite.py
+++ main_vsm_infinite.py
@@ -46,7 +46,7 @@ def main_vsm_infinite(user_params=None):
     ])
 
     #plott(testResults)
-    flatten_column_major(testResults)
+    #flatten_column_major(testResults)
 
     return testResults  # Now it only returns results without calling visualization
 
