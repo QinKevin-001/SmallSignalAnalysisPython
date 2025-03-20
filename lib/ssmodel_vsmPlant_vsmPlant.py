0c1d6a600b90a99fb87ae2f4c4d7f1902964f42c
QinKevin-001 <zihaoqin1via@gmail.com>
2025-03-19 23:53:05 -0400

    vsmPlant vsmPlant OutofOrder fix

--- lib/ssmodel_vsmPlant_vsmPlant.py
+++ lib/ssmodel_vsmPlant_vsmPlant.py
@@ -87,7 +87,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
 
     # Assemble the overall system matrix Asys using block matrices
     # Row 1
-    row1_block1 = A1 + Bw1 @ Cw1.T + B1 @ Ngen1 @ C1  # Use Cw1.T instead of Cw1
+    row1_block1 = A1 + np.outer(Bw1, Cw1) + B1 @ Ngen1 @ C1  # Use np.outer instead of matrix multiplication
     row1_block2 = np.zeros((21, 21))
     row1_block3 = B1 @ Nline1Gen
     row1_block4 = np.zeros((21, 2))
@@ -95,7 +95,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     row1 = np.hstack([row1_block1, row1_block2, row1_block3, row1_block4, row1_block5])
 
     # Row 2
-    row2_block1 = Bw2 @ Cw1.T  # Use Cw1.T instead of Cw1
+    row2_block1 = np.outer(Bw2, Cw1)  # Use np.outer instead of matrix multiplication
     row2_block2 = A2 + B2 @ Ngen2 @ C2
     row2_block3 = np.zeros((21, 2))
     row2_block4 = B2 @ Nline2Gen
@@ -103,7 +103,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     row2 = np.hstack([row2_block1, row2_block2, row2_block3, row2_block4, row2_block5])
 
     # Row 3
-    row3_block1 = Bwline1 @ Cw1.T + B1line1 @ Ngen1 @ C1  # Use Cw1.T instead of Cw1
+    row3_block1 = np.outer(Bwline1, Cw1) + B1line1 @ Ngen1 @ C1  # Use np.outer instead of matrix multiplication
     row3_block2 = np.zeros((2, 21))
     row3_block3 = Aline1 + B1line1 @ Nline1Gen + B2line1 @ Nline1Load
     row3_block4 = B2line1 @ Nline2Load
@@ -111,7 +111,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     row3 = np.hstack([row3_block1, row3_block2, row3_block3, row3_block4, row3_block5])
 
     # Row 4
-    row4_block1 = Bwline2 @ Cw1.T  # Use Cw1.T instead of Cw1
+    row4_block1 = np.outer(Bwline2, Cw1)  # Use np.outer instead of matrix multiplication
     row4_block2 = B1line2 @ Ngen2 @ C2
     row4_block3 = B2line2 @ Nline1Load
     row4_block4 = Aline2 + B1line2 @ Nline2Gen + B2line2 @ Nline2Load
@@ -119,7 +119,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     row4 = np.hstack([row4_block1, row4_block2, row4_block3, row4_block4, row4_block5])
 
     # Row 5
-    row5_block1 = Bwload @ Cw1.T  # Use Cw1.T instead of Cw1
+    row5_block1 = np.outer(Bwload, Cw1)  # Use np.outer instead of matrix multiplication
     row5_block2 = np.zeros((2, 21))
     row5_block3 = Bload @ Nline1Load
     row5_block4 = Bload @ Nline2Load
