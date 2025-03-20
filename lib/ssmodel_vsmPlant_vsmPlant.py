3f53b2ee4004f3464fc027ba5ee7dab9d3d3522b
QinKevin-001 <zihaoqin1via@gmail.com>
2025-03-19 23:57:16 -0400

    vsmPlant vsmPlant OutofOrder fix

--- lib/ssmodel_vsmPlant_vsmPlant.py
+++ lib/ssmodel_vsmPlant_vsmPlant.py
@@ -87,43 +87,43 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
 
     # Assemble the overall system matrix Asys using block matrices
     # Row 1
-    row1_block1 = A1 + np.outer(Bw1, Cw1) + B1 @ Ngen1 @ C1  # Use np.outer instead of matrix multiplication
+    row1_block1 = A1 + Bw1 @ Cw1 + B1 @ Ngen1 @ C1  # (21,21)
     row1_block2 = np.zeros((21, 21))
-    row1_block3 = B1 @ Nline1Gen
+    row1_block3 = B1 @ Nline1Gen  # (21,2)
     row1_block4 = np.zeros((21, 2))
     row1_block5 = np.zeros((21, 2))
     row1 = np.hstack([row1_block1, row1_block2, row1_block3, row1_block4, row1_block5])
 
     # Row 2
-    row2_block1 = np.outer(Bw2, Cw1)  # Use np.outer instead of matrix multiplication
-    row2_block2 = A2 + B2 @ Ngen2 @ C2
+    row2_block1 = Bw2 @ Cw1  # (21,21)
+    row2_block2 = A2 + B2 @ Ngen2 @ C2  # (21,21)
     row2_block3 = np.zeros((21, 2))
-    row2_block4 = B2 @ Nline2Gen
+    row2_block4 = B2 @ Nline2Gen  # (21,2)
     row2_block5 = np.zeros((21, 2))
     row2 = np.hstack([row2_block1, row2_block2, row2_block3, row2_block4, row2_block5])
 
     # Row 3
-    row3_block1 = np.outer(Bwline1, Cw1) + B1line1 @ Ngen1 @ C1  # Use np.outer instead of matrix multiplication
+    row3_block1 = Bwline1 @ Cw1 + B1line1 @ Ngen1 @ C1  # (2,21)
     row3_block2 = np.zeros((2, 21))
-    row3_block3 = Aline1 + B1line1 @ Nline1Gen + B2line1 @ Nline1Load
-    row3_block4 = B2line1 @ Nline2Load
-    row3_block5 = B2line1 @ Nload
+    row3_block3 = Aline1 + B1line1 @ Nline1Gen + B2line1 @ Nline1Load  # (2,2)
+    row3_block4 = B2line1 @ Nline2Load  # (2,2)
+    row3_block5 = B2line1 @ Nload  # (2,2)
     row3 = np.hstack([row3_block1, row3_block2, row3_block3, row3_block4, row3_block5])
 
     # Row 4
-    row4_block1 = np.outer(Bwline2, Cw1)  # Use np.outer instead of matrix multiplication
-    row4_block2 = B1line2 @ Ngen2 @ C2
-    row4_block3 = B2line2 @ Nline1Load
-    row4_block4 = Aline2 + B1line2 @ Nline2Gen + B2line2 @ Nline2Load
-    row4_block5 = B2line2 @ Nload
+    row4_block1 = Bwline2 @ Cw1  # (2,21)
+    row4_block2 = B1line2 @ Ngen2 @ C2  # (2,21)
+    row4_block3 = B2line2 @ Nline1Load  # (2,2)
+    row4_block4 = Aline2 + B1line2 @ Nline2Gen + B2line2 @ Nline2Load  # (2,2)
+    row4_block5 = B2line2 @ Nload  # (2,2)
     row4 = np.hstack([row4_block1, row4_block2, row4_block3, row4_block4, row4_block5])
 
     # Row 5
-    row5_block1 = np.outer(Bwload, Cw1)  # Use np.outer instead of matrix multiplication
+    row5_block1 = Bwload @ Cw1  # (2,21)
     row5_block2 = np.zeros((2, 21))
-    row5_block3 = Bload @ Nline1Load
-    row5_block4 = Bload @ Nline2Load
-    row5_block5 = Aload + Bload @ Nload
+    row5_block3 = Bload @ Nline1Load  # (2,2)
+    row5_block4 = Bload @ Nline2Load  # (2,2)
+    row5_block5 = Aload + Bload @ Nload  # (2,2)
     row5 = np.hstack([row5_block1, row5_block2, row5_block3, row5_block4, row5_block5])
 
     # Assemble overall system matrix
