38964ddb429ed758f33bdcb606f1e4fbd7e28338
QinKevin-001 <zihaoqin1via@gmail.com>
2025-03-19 23:51:33 -0400

    vsmPlant vsmPlant OutofOrder fix

--- lib/ssmodel_vsmPlant_vsmPlant.py
+++ lib/ssmodel_vsmPlant_vsmPlant.py
@@ -12,7 +12,8 @@ from lib.ssmodel_load import ssmodel_load
 from lib.eigenvalue_analysis import eigenvalue_analysis
 
 
-def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine2, parasLoad, dominantParticipationFactorBoundary):
+def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine2, parasLoad,
+                              dominantParticipationFactorBoundary):
     ## Power Flow Calculation
     x0 = np.array([1, 0, 0, 0, 1, 1, 1, 1])
     x, info, ier, msg = fsolve(
@@ -22,7 +23,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
         maxfev=500,
         full_output=True
     )
-    pfExitFlag = ier
+    pfExitFlag = ier  # Convergence status of fsolve
 
     # Calculate Power Flow outputs
     w, V1, V2, V3, Io1, Io2 = pf_calc_ibrPlant_ibrPlant(x, parasLine1, parasLine2, parasLoad)
@@ -48,7 +49,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     stateMatrixLine2 = ssmodel_line(wbase, parasLine2, steadyStateValuesXLine2, steadyStateValuesULine2)
     stateMatrixLoad = ssmodel_load(wbase, parasLoad, steadyStateValuesXLoad, steadyStateValuesULoad)
 
-    # Extract matrices
+    # Extract matrices from state-space models
     A1 = stateMatrix1['A']
     B1 = stateMatrix1['B']
     Bw1 = stateMatrix1['Bw']
@@ -74,7 +75,7 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     Bload = stateMatrixLoad['B']
     Bwload = stateMatrixLoad['Bw']
 
-    # Define coupling matrices
+    # Define coupling matrices based on parasLoad parameters
     Rx = parasLoad['Rx']
     Ngen1 = Rx * np.eye(2)
     Nline1Gen = -Rx * np.eye(2)
@@ -84,69 +85,74 @@ def ssmodel_vsmPlant_vsmPlant(wbase, parasIBR1, parasIBR2, parasLine1, parasLine
     Nline2Load = Rx * np.eye(2)
     Nload = -Rx * np.eye(2)
 
-    # Assemble system matrix using block matrix operations similar to case 12
-    block11 = A1 + Bw1 @ Cw1.T + B1 @ Ngen1 @ C1
-    block12 = np.zeros((21, 21))
-    block13 = B1 @ Nline1Gen
-    block14 = np.zeros((21, 2))
-    block15 = np.zeros((21, 2))
-    row1 = np.hstack([block11, block12, block13, block14, block15])
+    # Assemble the overall system matrix Asys using block matrices
+    # Row 1
+    row1_block1 = A1 + Bw1 @ Cw1.T + B1 @ Ngen1 @ C1  # Use Cw1.T instead of Cw1
+    row1_block2 = np.zeros((21, 21))
+    row1_block3 = B1 @ Nline1Gen
+    row1_block4 = np.zeros((21, 2))
+    row1_block5 = np.zeros((21, 2))
+    row1 = np.hstack([row1_block1, row1_block2, row1_block3, row1_block4, row1_block5])
 
-    block21 = Bw2 @ Cw1.T
-    block22 = A2 + B2 @ Ngen2 @ C2
-    block23 = np.zeros((21, 2))
-    block24 = B2 @ Nline2Gen
-    block25 = np.zeros((21, 2))
-    row2 = np.hstack([block21, block22, block23, block24, block25])
+    # Row 2
+    row2_block1 = Bw2 @ Cw1.T  # Use Cw1.T instead of Cw1
+    row2_block2 = A2 + B2 @ Ngen2 @ C2
+    row2_block3 = np.zeros((21, 2))
+    row2_block4 = B2 @ Nline2Gen
+    row2_block5 = np.zeros((21, 2))
+    row2 = np.hstack([row2_block1, row2_block2, row2_block3, row2_block4, row2_block5])
 
-    block31 = Bwline1 @ Cw1.T + B1line1 @ Ngen1 @ C1
-    block32 = np.zeros((2, 21))
-    block33 = Aline1 + B1line1 @ Nline1Gen + B2line1 @ Nline1Load
-    block34 = B2line1 @ Nline2Load
-    block35 = B2line1 @ Nload
-    row3 = np.hstack([block31, block32, block33, block34, block35])
+    # Row 3
+    row3_block1 = Bwline1 @ Cw1.T + B1line1 @ Ngen1 @ C1  # Use Cw1.T instead of Cw1
+    row3_block2 = np.zeros((2, 21))
+    row3_block3 = Aline1 + B1line1 @ Nline1Gen + B2line1 @ Nline1Load
+    row3_block4 = B2line1 @ Nline2Load
+    row3_block5 = B2line1 @ Nload
+    row3 = np.hstack([row3_block1, row3_block2, row3_block3, row3_block4, row3_block5])
 
-    block41 = Bwline2 @ Cw1.T
-    block42 = B1line2 @ Ngen2 @ C2
-    block43 = B2line2 @ Nline1Load
-    block44 = Aline2 + B1line2 @ Nline2Gen + B2line2 @ Nline2Load
-    block45 = B2line2 @ Nload
-    row4 = np.hstack([block41, block42, block43, block44, block45])
+    # Row 4
+    row4_block1 = Bwline2 @ Cw1.T  # Use Cw1.T instead of Cw1
+    row4_block2 = B1line2 @ Ngen2 @ C2
+    row4_block3 = B2line2 @ Nline1Load
+    row4_block4 = Aline2 + B1line2 @ Nline2Gen + B2line2 @ Nline2Load
+    row4_block5 = B2line2 @ Nload
+    row4 = np.hstack([row4_block1, row4_block2, row4_block3, row4_block4, row4_block5])
 
-    block51 = Bwload @ Cw1.T
-    block52 = np.zeros((2, 21))
-    block53 = Bload @ Nline1Load
-    block54 = Bload @ Nline2Load
-    block55 = Aload + Bload @ Nload
-    row5 = np.hstack([block51, block52, block53, block54, block55])
+    # Row 5
+    row5_block1 = Bwload @ Cw1.T  # Use Cw1.T instead of Cw1
+    row5_block2 = np.zeros((2, 21))
+    row5_block3 = Bload @ Nline1Load
+    row5_block4 = Bload @ Nline2Load
+    row5_block5 = Aload + Bload @ Nload
+    row5 = np.hstack([row5_block1, row5_block2, row5_block3, row5_block4, row5_block5])
 
-    # Assemble final system matrix
+    # Assemble overall system matrix
     Asys = np.vstack([row1, row2, row3, row4, row5])
+    # Asys dimensions before deletion: (48,48)
 
-    # State variable labels
+    ## State Variable Labels
     ssVar1 = np.array(stateMatrix1['ssVariables']).flatten()
     ssVar2 = np.array(stateMatrix2['ssVariables']).flatten()
     ssVarLine1 = np.array(stateMatrixLine1['ssVariables']).flatten()
     ssVarLine2 = np.array(stateMatrixLine2['ssVariables']).flatten()
     ssVarLoad = np.array(stateMatrixLoad['ssVariables']).flatten()
 
+    # Build labels dynamically based on the lengths of each state variable array
     labels = (['IBR1'] * len(ssVar1) +
               ['IBR2'] * len(ssVar2) +
               ['Line1'] * len(ssVarLine1) +
               ['Line2'] * len(ssVarLine2) +
               ['Load'] * len(ssVarLoad))
+    ssVariables = np.column_stack((np.concatenate([ssVar1, ssVar2, ssVarLine1, ssVarLine2, ssVarLoad]),
+                                   np.array(labels, dtype=object)))
+    # ssVariables dimensions before deletion: (50,2)  (for example)
 
-    ssVariables = np.column_stack((
-        np.concatenate([ssVar1, ssVar2, ssVarLine1, ssVarLine2, ssVarLoad]),
-        np.array(labels, dtype=object)
-    ))
-
-    # Remove the 10th row and column
-    Asys = np.delete(Asys, 9, axis=0)
-    Asys = np.delete(Asys, 9, axis=1)
+    # Remove the 10th row and 10th column from Asys and the corresponding state variable
+    Asys = np.delete(Asys, 9, axis=0)  # Remove row with index 9 (10th row)
+    Asys = np.delete(Asys, 9, axis=1)  # Remove column with index 9 (10th column)
     ssVariables = np.delete(ssVariables, 9, axis=0)
 
-    # Eigenvalue Analysis
+    ## Eigenvalue Analysis
     eigenvalueAnalysisResults = eigenvalue_analysis(Asys, ssVariables, dominantParticipationFactorBoundary)
 
     return Asys, steadyStateValuesX, eigenvalueAnalysisResults, pfExitFlag
