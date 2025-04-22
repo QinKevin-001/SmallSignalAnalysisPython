f3ea33c3c8a13691962db81e0e8988137d880df3
QinKevin-001 <zihaoqin1via@gmail.com>
2025-04-22 16:51:39 -0400

    Case11 vis updated

--- Main/case11main_droopPlant_vsmPlant.py
+++ Main/case11main_droopPlant_vsmPlant.py
@@ -68,29 +68,16 @@ def main_droopPlant_vsmPlant(user_params=None):
 
     # If user-defined parameters exist, update the default dictionary
     if user_params:
-        for key in parasIBR1.keys():
-            if key in user_params:
-                parasIBR1[key] = user_params[key]
-
-    if user_params:
-        for key in parasIBR2.keys():
-            if key in user_params:
-                parasIBR2[key] = user_params[key]
-
-    if user_params:
-        for key in parasLine1.keys():
-            if key in user_params:
-                parasLine1[key] = user_params[key]
-
-    if user_params:
-        for key in parasLine2.keys():
-            if key in user_params:
-                parasLine2[key] = user_params[key]
-
-    if user_params:
-        for key in parasLoad.keys():
-            if key in user_params:
-                parasLoad[key] = user_params[key]
+        if 'parasIBR1' in user_params:
+            parasIBR1.update({k: float(v) for k, v in user_params['parasIBR1'].items()})
+        if 'parasIBR2' in user_params:
+            parasIBR2.update({k: float(v) for k, v in user_params['parasIBR2'].items()})
+        if 'parasLine1' in user_params:
+            parasLine1.update({k: float(v) for k, v in user_params['parasLine1'].items()})
+        if 'parasLine2' in user_params:
+            parasLine2.update({k: float(v) for k, v in user_params['parasLine2'].items()})
+        if 'parasLoad' in user_params:
+            parasLoad.update({k: float(v) for k, v in user_params['parasLoad'].items()})
 
     # Column Names
     testResults = [["Parameter", "Eigenvalues", "maxRealValue", "minDampingRatio", "modalAnalysis", "pfExitFlag"]]
