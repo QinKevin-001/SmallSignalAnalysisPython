8504df1052b69741f6e526a8202cc34e2b135f4f
QinKevin-001 <zihaoqin1via@gmail.com>
2025-08-04 10:39:53 -0700

    text sizing changes

--- Visualization/case10vis_droop_vsm.py
+++ Visualization/case10vis_droop_vsm.py
@@ -231,7 +231,7 @@ def visualization(testResults):
             xaxis_title_font=dict(size=30),
             yaxis_title_font=dict(size=30),
             xaxis=dict(tickfont=dict(size=20)),
-            yaxis=dict(tickfont=dict(size=20))
+            yaxis=dict(tickfont=dict(size=10))
         )
         heatmap_fig.update_xaxes(tickmode='linear', tick0=1, dtick=1)
         st.plotly_chart(heatmap_fig, use_container_width=True)
